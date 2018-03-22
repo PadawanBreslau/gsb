de1320125977ae339640a51aaa2cac9f63ef1126
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-03-22 15:22:10 +0100

    Revert "Add facebook widget"
    This reverts commit 6d0d3216c209566281ec026969645055997fac87.

--- dist/js/fb.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$('#face-slider').hover(
-		function(){	$('#face-slider').stop().animate({"left": "0"}, 1000); } ,
-		function(){ $('#face-slider').stop().animate({"left": "-292px"}, 1000); }
-	);
-});
